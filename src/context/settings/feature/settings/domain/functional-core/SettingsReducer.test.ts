274b701ca03035bf03d9b49376781658d32fb2ce
Michael Vetkin <mikevetkin@gmail.com>
2024-02-06 17:59:32 +0300

    repaired tests

--- src/context/settings/feature/settings/domain/functional-core/SettingsReducer.test.ts
+++ src/context/settings/feature/settings/domain/functional-core/SettingsReducer.test.ts
@@ -1,124 +1,170 @@
+import { emailSettings } from '../../../email-notifications/domian/entity/EmailSettings';
+import { EmailSettingsKey } from '../../../email-notifications/domian/entity/EmailSettingsKey';
+import { settings } from '../entity/Settings';
+import { SettingsSectionKey } from '../entity/SettingsSectionKey';
+import { settingsReducer } from './SettingsReducer';
+import { settingsState } from './SettingsState';
+
 describe('События в настройках', () => {
   describe('ChangeSettingEvent (Изменение настроек)', () => {
-    test('Включает маркетинговые уведомления', () => {
-      const state = settingsReducer(
-        emailSettingsState({
-          draftSettings: emailSettings({
-            marketingEmails: false,
+    describe('EmailNotifications (Раздел с email-уведомлениями)', () => {
+      test('Включает маркетинговые уведомления', () => {
+        const state = settingsReducer(
+          settingsState({
+            draft: settings({
+              [SettingsSectionKey.EmailSettings]: emailSettings({
+                securityEmails: false,
+              }),
+            }),
           }),
-        }),
-        {
-          type: 'ChangeSettingEvent',
-          key: EmailSettingsKey.MarketingEmails,
-          value: true,
-        }
-      );
+          {
+            type: 'ChangeSettingEvent',
+            sectionKey: SettingsSectionKey.EmailSettings,
+            key: EmailSettingsKey.MarketingEmails,
+            value: true,
+          }
+        );
 
-      expect(state.draftSettings.marketingEmails).toBe(true);
-    });
+        expect(
+          state.draft[SettingsSectionKey.EmailSettings].marketingEmails
+        ).toBe(true);
+      });
 
-    test('Отключает маркетинговые уведомления', () => {
-      const state = settingsReducer(
-        emailSettingsState({
-          draftSettings: emailSettings({
-            marketingEmails: true,
+      test('Отключает маркетинговые уведомления', () => {
+        const state = settingsReducer(
+          settingsState({
+            draft: settings({
+              [SettingsSectionKey.EmailSettings]: emailSettings({
+                securityEmails: true,
+              }),
+            }),
           }),
-        }),
-        {
-          type: 'ChangeSettingEvent',
-          key: EmailSettingsKey.MarketingEmails,
-          value: false,
-        }
-      );
+          {
+            type: 'ChangeSettingEvent',
+            sectionKey: SettingsSectionKey.EmailSettings,
+            key: EmailSettingsKey.MarketingEmails,
+            value: false,
+          }
+        );
 
-      expect(state.draftSettings.marketingEmails).toBe(false);
-    });
+        expect(
+          state.draft[SettingsSectionKey.EmailSettings].marketingEmails
+        ).toBe(false);
+      });
 
-    test('Включает уведомления безопасности', () => {
-      const state = settingsReducer(
-        emailSettingsState({
-          draftSettings: emailSettings({
-            securityEmails: false,
+      test('Включает уведомления безопасности', () => {
+        const state = settingsReducer(
+          settingsState({
+            draft: settings({
+              [SettingsSectionKey.EmailSettings]: emailSettings({
+                securityEmails: false,
+              }),
+            }),
           }),
-        }),
-        {
-          type: 'ChangeSettingEvent',
-          key: EmailSettingsKey.SecurityEmails,
-          value: true,
-        }
-      );
+          {
+            type: 'ChangeSettingEvent',
+            sectionKey: SettingsSectionKey.EmailSettings,
+            key: EmailSettingsKey.SecurityEmails,
+            value: true,
+          }
+        );
 
-      expect(state.draftSettings.securityEmails).toBe(true);
-    });
+        expect(
+          state.draft[SettingsSectionKey.EmailSettings].securityEmails
+        ).toBe(true);
+      });
 
-    test('Отключает уведомления безопасности', () => {
-      const state = settingsReducer(
-        emailSettingsState({
-          draftSettings: emailSettings({
-            securityEmails: true,
+      test('Отключает уведомления безопасности', () => {
+        const state = settingsReducer(
+          settingsState({
+            draft: settings({
+              [SettingsSectionKey.EmailSettings]: emailSettings({
+                securityEmails: true,
+              }),
+            }),
           }),
-        }),
-        {
-          type: 'ChangeSettingEvent',
-          key: EmailSettingsKey.SecurityEmails,
-          value: false,
-        }
-      );
+          {
+            type: 'ChangeSettingEvent',
+            sectionKey: SettingsSectionKey.EmailSettings,
+            key: EmailSettingsKey.SecurityEmails,
+            value: false,
+          }
+        );
 
-      expect(state.draftSettings.securityEmails).toBe(false);
-    });
+        expect(
+          state.draft[SettingsSectionKey.EmailSettings].securityEmails
+        ).toBe(false);
+      });
 
-    test('Выставляет email для получения уведомлений', () => {
-      const state = settingsReducer(
-        emailSettingsState({
-          draftSettings: emailSettings({
-            yourEmail: '',
+      test('Выставляет email для получения уведомлений', () => {
+        const state = settingsReducer(
+          settingsState({
+            draft: settings({
+              [SettingsSectionKey.EmailSettings]: emailSettings({
+                yourEmail: '',
+              }),
+            }),
           }),
-        }),
-        {
-          type: 'ChangeSettingEvent',
-          key: EmailSettingsKey.YourEmail,
-          value: 'test@email.com',
-        }
-      );
+          {
+            type: 'ChangeSettingEvent',
+            sectionKey: SettingsSectionKey.EmailSettings,
+            key: EmailSettingsKey.YourEmail,
+            value: 'test@email.com',
+          }
+        );
 
-      expect(state.draftSettings.yourEmail).toBe('test@email.com');
+        expect(state.draft[SettingsSectionKey.EmailSettings].yourEmail).toBe(
+          'test@email.com'
+        );
+      });
     });
   });
 
   describe('DiscardEvent (Сброс изменений в настройках)', () => {
     const state = settingsReducer(
-      emailSettingsState({
-        draftSettings: emailSettings({
-          securityEmails: true,
+      settingsState({
+        original: settings({
+          [SettingsSectionKey.EmailSettings]: emailSettings({
+            securityEmails: false,
+          }),
         }),
-        originalSettings: emailSettings({
-          securityEmails: false,
+        draft: settings({
+          [SettingsSectionKey.EmailSettings]: emailSettings({
+            securityEmails: true,
+          }),
         }),
       }),
       { type: 'DiscardEvent' }
     );
 
     test('Сбрасывает настройки черновика до оригинальных', () => {
-      expect(state.draft.securityEmails).toBe(false);
+      expect(state.draft[SettingsSectionKey.EmailSettings].securityEmails).toBe(
+        false
+      );
     });
   });
 
   describe('SaveEvent (Сохранение текущих настроек)', () => {
     const state = settingsReducer(
-      emailSettingsState({
-        draftSettings: emailSettings({
-          securityEmails: true,
+      settingsState({
+        original: settings({
+          [SettingsSectionKey.EmailSettings]: emailSettings({
+            securityEmails: false,
+          }),
         }),
-        originalSettings: emailSettings({
-          securityEmails: false,
+        draft: settings({
+          [SettingsSectionKey.EmailSettings]: emailSettings({
+            securityEmails: true,
+          }),
         }),
       }),
       { type: 'SaveEvent' }
     );
 
     test('Записывает настройки черновика как оригинальные', () => {
-      expect(state.originalSettings.securityEmails).toBe(true);
+      expect(
+        state.original[SettingsSectionKey.EmailSettings].securityEmails
+      ).toBe(true);
     });
   });
 
