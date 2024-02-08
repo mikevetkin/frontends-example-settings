138e56fd0188a9b76de8305697bcd69657e25ebb
Michael Vetkin <mikevetkin@gmail.com>
2024-02-08 22:00:08 +0300

    Deleted dependent place

--- src/context/settings/feature/settings/ui/components/Section/sectionPresentation.ts
+++ src/context/settings/feature/settings/ui/components/Section/sectionPresentation.ts
@@ -1,6 +1,5 @@
+import { SettingsKey } from '../../../domain/entity/SettingsKey';
 import { SettingsSectionKey } from '../../../domain/entity/SettingsSectionKey';
-import { EmailSettingsKey } from '../../../domain/entity/email-notifications/EmailSettingsKey';
-import { PersonalSettingsKey } from '../../../domain/entity/personal/PersonalSettingsKey';
 import { SettingsState } from '../../../domain/functional-core/SettingsState';
 import { mapSettingsSectionHeading } from '../../../domain/mapper/mapSettingsSectionHeading';
 import { settingPresentation } from '../Setting/settingPresentation';
@@ -12,10 +11,8 @@ export const sectionPresentation = (
 ): SectionViewState => {
   return new SectionViewState({
     heading: mapSettingsSectionHeading[section],
-    list: (
-      Object.keys(state.draft[section]) as
-        | EmailSettingsKey[]
-        | PersonalSettingsKey[]
-    ).map((setting) => settingPresentation(state, section, setting)),
+    list: (Object.keys(state.draft[section]) as SettingsKey[]).map((setting) =>
+      settingPresentation(state, section, setting)
+    ),
   });
 };
