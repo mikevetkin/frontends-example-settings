2859742dcd9f26b00d9d7fef5042455585801297
Michael Vetkin <mikevetkin@gmail.com>
2024-02-08 21:58:56 +0300

    Added skeleton for sections

--- src/context/settings/feature/settings/ui/pages/settingsPagePresentation.ts
+++ src/context/settings/feature/settings/ui/pages/settingsPagePresentation.ts
@@ -1,14 +1,27 @@
+import { SkeletonListViewState } from '@/core/view-state/SkeletonListViewState';
 import { SettingsSectionKey } from '../../domain/entity/SettingsSectionKey';
 import { SettingsState } from '../../domain/functional-core/SettingsState';
 import { sectionPresentation } from '../components/Section/sectionPresentation';
 import { SettingsPageViewState } from './SettingsPageViewState';
+import { SkeletonViewState } from '@/core/view-state/SkeletonViewState';
 
 export const settingsPagePresentation = (state: SettingsState) => {
-  const { draft } = state;
+  const { draft, status } = state;
 
   return new SettingsPageViewState({
-    sections: (Object.keys(draft) as SettingsSectionKey[]).map((section) =>
-      sectionPresentation(state, section)
-    ),
+    sections: (Object.keys(draft) as SettingsSectionKey[]).map((section) => {
+      switch (status) {
+        case 'loading':
+          return new SkeletonListViewState({
+            list: Object.values(draft[section]).map(
+              () => new SkeletonViewState()
+            ),
+          });
+        case 'idle':
+          return sectionPresentation(state, section);
+        case 'pending':
+          return sectionPresentation(state, section);
+      }
+    }),
   });
 };
