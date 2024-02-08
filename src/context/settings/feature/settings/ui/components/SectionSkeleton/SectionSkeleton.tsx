3d498ee8b546c0ddeead0794f618305f52bd19ab
Michael Vetkin <mikevetkin@gmail.com>
2024-02-09 01:04:19 +0300

    Adde key to skeleton

--- src/context/settings/feature/settings/ui/components/SectionSkeleton/SectionSkeleton.tsx
+++ src/context/settings/feature/settings/ui/components/SectionSkeleton/SectionSkeleton.tsx
@@ -10,8 +10,8 @@ export const SectionSkeleton: React.FC<SkeletonSectionProps> = ({
 }) => (
   <div className="flex flex-col gap-4">
     <Skeleton className="h-[20px] w-[100px] rounded-full" />
-    {viewState.list.map(() => (
-      <Skeleton className="h-[76px] w-[100%]" />
+    {viewState.list.map((_, key) => (
+      <Skeleton className="h-[76px] w-[100%]" key={key} />
     ))}
   </div>
 );
