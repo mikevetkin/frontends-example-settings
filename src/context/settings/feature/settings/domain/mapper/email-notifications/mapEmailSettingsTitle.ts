eb8add56216f950d0e37624e2b8ed0f6f124b350
Michael Vetkin <mikevetkin@gmail.com>
2024-02-13 16:59:47 +0300

    Deleted exccess code

--- src/context/settings/feature/settings/domain/mapper/email-notifications/mapEmailSettingsTitle.ts
+++ src/context/settings/feature/settings/domain/mapper/email-notifications/mapEmailSettingsTitle.ts
@@ -1,8 +1,6 @@
 import { EmailSettingsKey } from '../../entity/email-notifications/EmailSettingsKey';
 
 export const mapEmailSettingsTitle: Record<EmailSettingsKey, string> = {
-  [EmailSettingsKey.YourEmail]: 'Your Email',
   [EmailSettingsKey.MarketingEmails]: 'Marketing emails',
   [EmailSettingsKey.SecurityEmails]: 'Security emails',
-  [EmailSettingsKey.NewFeatureEmails]: 'New Feature emails',
 };
