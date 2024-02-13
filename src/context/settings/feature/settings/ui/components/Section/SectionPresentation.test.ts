86c47f1b59ba146a30a2874e71e164a4648e443b
Michael Vetkin <mikevetkin@gmail.com>
2024-02-13 13:13:07 +0300

    Deleted trivial tests, added new test case for settings control

--- src/context/settings/feature/settings/ui/components/Section/SectionPresentation.test.ts
+++
@@ -1,6 +0,0 @@
-// import { settings } from '../../../domain/entity/Settings';
-// import { SettingsSectionKey } from '../../../domain/entity/SettingsSectionKey';
-// import { emailSettings } from '../../../domain/entity/email-notifications/EmailSettings';
-// import { settingsState } from '../../../domain/functional-core/SettingsState';
-// import { SaveOrDiscardViewState } from '../SaveOrDiscard/SaveOrDiscardViewState';
-// import { sectionPresentation } from './SectionPresentation';
