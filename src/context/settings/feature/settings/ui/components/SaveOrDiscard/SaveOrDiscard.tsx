ad012c3c42ad0dbbf9cbd3b4a20e5ad51e051f1a
Michael Vetkin <mikevetkin@gmail.com>
2024-02-14 13:08:48 +0300

    Added absolute position for save discard

--- src/context/settings/feature/settings/ui/components/SaveOrDiscard/SaveOrDiscard.tsx
+++ src/context/settings/feature/settings/ui/components/SaveOrDiscard/SaveOrDiscard.tsx
@@ -15,20 +15,30 @@ export const SaveOrDiscard: React.FC<SaveOrDiscardProps> = ({
 }) => {
   return (
     <>
-      <div className="flex justify-end gap-2">
-        <Button
-          variant="secondary"
-          disabled={viewState.discard.disabled}
-          onClick={onClickDiscard}
-        >
-          Discard
-        </Button>
-        <Button disabled={viewState.save.disabled} onClick={onClickSave}>
-          {viewState.save.icon && (
-            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-          )}
-          {viewState.save.label}
-        </Button>
+      <div className="flex justify-between gap-2">
+        <div className="flex flex-col items-start">
+          <h5 className="mb-1 font-medium leading-none tracking-tight">
+            Changes detected
+          </h5>
+          <div className="text-sm [&_p]:leading-relaxed">
+            Would you like to save?
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <Button
+            variant="secondary"
+            disabled={viewState.discard.disabled}
+            onClick={onClickDiscard}
+          >
+            Discard
+          </Button>
+          <Button disabled={viewState.save.disabled} onClick={onClickSave}>
+            {viewState.save.icon && (
+              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            {viewState.save.label}
+          </Button>
+        </div>
       </div>
     </>
   );
--- src/context/settings/feature/settings/ui/pages/SettingsPage.tsx
+++ src/context/settings/feature/settings/ui/pages/SettingsPage.tsx
@@ -26,17 +26,19 @@ export const SettingsPage: React.FC = () => {
   };
 
   return (
-    <div className="w-full space-y-6">
+    <div className="relative h-[100vh] w-full space-y-6 p-8">
       <h1 className="mb-4 text-left text-4xl font-medium">Settings</h1>
       <LazyToDoSection />
       {viewState.sections.map((section) => renderSection(section))}
       <LazyToDoSection />
       {viewState.actions && (
-        <SaveOrDiscard
-          viewState={viewState.actions}
-          onClickSave={saveSettings}
-          onClickDiscard={discardSettings}
-        />
+        <div className="absolute bottom-8 left-8 right-8 rounded-lg border bg-background px-3 py-3 text-sm">
+          <SaveOrDiscard
+            viewState={viewState.actions}
+            onClickSave={saveSettings}
+            onClickDiscard={discardSettings}
+          />
+        </div>
       )}
     </div>
   );
